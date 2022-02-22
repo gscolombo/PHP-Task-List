522db99bb441d4d11784a397b5a19002f852a65b
Gabriel <gabriel.souza.colombo13@gmail.com>
2022-02-22 19:16:31 -0300

    Refatoração da função para visualização de detalhes de tarefas

--- js/modules/showTaskDetails.js
+++ js/modules/showTaskDetails.js
@@ -1,34 +1,24 @@
-export default function showDetails() {
-    const tasks = Array.from(document.querySelectorAll("div.task"));
+export default function showDetails(e) {
     const blurRect = document.createElement("div");
     blurRect.classList.add("blur-rect");
 
-    function handleClick(e) {
-        const btn = e.currentTarget;
-        const task = btn.parentElement.parentElement.cloneNode(true);
-        const btnClone = task.querySelector("button.details-btn");
+    const btn = e.currentTarget;
+    const task = btn.parentElement.parentElement.cloneNode(true);
+    const btnClone = task.querySelector("button.details-btn");
 
-        task.classList.add("show-details");
-        task.querySelector(".details").classList.remove("unshow");
-        btnClone.innerText = "Voltar";
-        
-        blurRect.append(task);
-        document.body.insertBefore(blurRect, document.querySelector("script"));
+    task.classList.add("show-details");
+    task.querySelector(".details").classList.remove("unshow");
+    btnClone.innerText = "Voltar";
+    
+    blurRect.append(task);
+    document.body.insertBefore(blurRect, document.querySelector("script"));
 
-        btnClone.addEventListener("click", () => {
-            task.classList.remove("show-details");
-            task.classList.add("unshow-details");
-            setTimeout(() => {
-                blurRect.removeChild(task);
-                blurRect.remove();
-            }, 250)
-            
-        })
-    }
-
-    tasks.forEach( task => {
-        const btn = task.querySelector(".options button.details-btn");
-        btn.addEventListener("click", handleClick);
+    btnClone.addEventListener("click", () => {
+        task.classList.remove("show-details");
+        task.classList.add("unshow-details");
+        setTimeout(() => {
+            blurRect.removeChild(task);
+            blurRect.remove();
+        }, 250)  
     })
-
 }
